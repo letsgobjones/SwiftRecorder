f0b9870a7b7138b395ecb43a13028d146a201693
Brandon <89342970+letsgobjones@users.noreply.github.com>
2025-07-02 16:13:53 +0900

    Refactored SwiftRecorderApp initialization to use PreviewContaine

--- SwiftRecorder/SwiftRecorderApp.swift
+++ SwiftRecorder/SwiftRecorderApp.swift
@@ -10,23 +10,44 @@ import SwiftData
 
 @main
 struct SwiftRecorderApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+  
+  
+  // Define the persistent ModelContainer (SHARED INSTANCE)
+ var sharedModelContainer: ModelContainer
+  
+  // Create the AppManager container instance as State
+  @State private var appManager: AppManager
+  
+  @MainActor  // ADD this attribute
+  init() {
+    print("SwiftRecrorder init() called...")
+    let schema = Schema([
+      RecordingSession.self, TranscriptionSegment.self,
 
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
+    ])
+
+    do {
+      #if !targetEnvironment(simulator)
+        // Configure for persistent storage (on-disk)
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        sharedModelContainer = try ModelContainer(for: schema, configurations: [modelConfiguration])
+      #else
+        // Use the PreviewContainer in the simulator, in memory only
+        sharedModelContainer = PreviewContainer.shared
+      #endif
+    } catch {
+      fatalError("Could not create ModelContainer: \(error)")
+    }
+
+    _appManager = State(initialValue: AppManager(modelContainer: sharedModelContainer))
+    print("SwiftRecrorder init finished.")
+  }
 
     var body: some Scene {
         WindowGroup {
             ContentView()
+            .environment(appManager)
+            .modelContainer(sharedModelContainer)
         }
-        .modelContainer(sharedModelContainer)
     }
 }
