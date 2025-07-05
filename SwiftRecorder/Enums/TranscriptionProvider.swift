4ac72887b37ea129cf51c0d68b0ee09415f32a7d
Brandon <89342970+letsgobjones@users.noreply.github.com>
2025-07-05 16:53:00 +0900

    Created TranscriptionProvider

---
+++ SwiftRecorder/Enums/TranscriptionProvider.swift
@@ -0,0 +1,16 @@
+//
+//  TranscriptionProvider.swift
+//  SwiftRecorder
+//
+//  Created by Brandon Jones on 7/5/25.
+//
+
+import Foundation
+
+
+// Defines the available transcription services
+enum TranscriptionProvider {
+    case appleOnDevice
+    // case google
+    // case whisper
+}
